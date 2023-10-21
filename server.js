69fdc737145fb77737d785901820ef6839da3261
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-22 00:35:23 +0700

    feat: Add request listener on the server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 console.log("Hello world i'm about to become a great cloud architect!");
+
+const http = require('http');
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    response.end('<h1>Halo, This is HTTP Server!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server is currently runniong on http://${host}:${port}/`)
+});
