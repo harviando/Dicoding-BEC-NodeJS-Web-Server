8e89b94c4433decbc755f51a3e1f4153f2f04e4b
Muhammad Harviando <harviandomuhammad@gmail.com>
2023-10-22 00:51:46 +0700

    feat: Add request method to handle response

--- server.js
+++ server.js
@@ -4,14 +4,27 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo, This is HTTP Server!</h1>');
+
+    const {method} = request;
+
+    if (method === 'GET'){
+        response.end('This is GET Response');
+    }
+    if (method === 'POST'){
+        response.end('This is POST Response');
+    }
+    if (method === 'PUT'){
+        response.end('This is PUT Response');
+    }
+    if (method === 'DELETE'){
+        response.end('This is DELETE Response');
+    }
 };
 
 const server = http.createServer(requestListener);
 
-const port = 5000;
+const port = 5001;
 const host = 'localhost';
 
 server.listen(port, host, () => {
